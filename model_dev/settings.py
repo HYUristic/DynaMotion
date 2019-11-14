cf0b5d505765a9144ea346d9c88854873ebb3e3a
James Youngchae Chee <litcoderr@gmail.com>
2019-11-15 00:18:22 +0900

    add: dataset downloading functionality from cloud

--- model_dev/settings.py
+++ model_dev/settings.py
@@ -1,9 +1,10 @@
 class Settings():
-    # Dataset Settings
-    dataset_root = '../../Dataset'
-    dataset_info = {
-        'Maestro' : {
-            'path' : './Maestro',
-            'link' : '' #TODO upload maestro dataset to drive and update link
+    def __init__(self):
+        # Dataset Settings
+        self.dataset_root = '../../DataHub'
+        self.dataset_info = {
+            'Maestro' : {
+                'path' : './maestro-v2.0.0',
+                'link' : '1BKfsI61Y9kZj1RETksJDUhr4UqAuh9VQ'
+            }
         }
-    }
--- model_dev/utils/datasets.py
+++ model_dev/utils/datasets.py
@@ -2,18 +2,32 @@ from model_dev.settings import Settings
 from model_dev.utils.google_drive import download_file_from_google_drive
 
 import os
+import zipfile
 from torch.utils.data import Dataset
 
 class Dataset_Backbone(Dataset):
     def __init__(self, settings: Settings, dataset_id: str):
-        self.settings = Settings
+        self.settings = settings
         dataset_path = os.path.join(self.settings.dataset_root, self.settings.dataset_info[dataset_id]['path'])
 
         # Validate Dataset
+        if not os.path.exists(self.settings.dataset_root):
+            os.mkdir(self.settings.dataset_root)
+
         if not os.path.exists(dataset_path):
-            print("Downloading '{dataset_id}'dataset from cloud...".format(dataset_id=dataset_id))
             link = self.settings.dataset_info[dataset_id]['link']
-            self.download_dataset(dataset_path=dataset_path, link=link)
+            print("Downloading '{dataset_id}' dataset from cloud... id:[{link}]".format(dataset_id=dataset_id, link=link))
+            comp_file_name = self.download_dataset(dataset_path=self.settings.dataset_root, link=link)
+
+            print("Unzipping...".format(dataset_id=dataset_id))
+            with zipfile.ZipFile(comp_file_name, 'r') as zip_ref:
+                zip_ref.extractall(self.settings.dataset_root)
+
+            if os.path.exists(dataset_path):
+                print("Successfully downloaded '{dataset_id}' dataset @ [{dataset_path}]".format(dataset_id=dataset_id, dataset_path=dataset_path))
+                os.remove(comp_file_name)
+            else:
+                raise Exception('dataset_path does not match downloaded dataset.. please check settings')
 
     def __getitem__(self, item: int):
         raise NotImplementedError
@@ -22,8 +36,9 @@ class Dataset_Backbone(Dataset):
         raise NotImplementedError
 
     def download_dataset(self, dataset_path: str, link: str):
-        #TODO Download via download_file_from_google_drive
-        pass
+        file_name = os.path.join(dataset_path, 'compressed.zip')
+        download_file_from_google_drive(id=link, destination=file_name)
+        return file_name
 
 
 class MaestroDataset(Dataset_Backbone):
